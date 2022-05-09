0b5c1f498ce3d17c7c7433ed4c1a85dcf251ddf8
first3numbers <marcelohac2016@gmail.com>
2022-05-09 20:50:10 -0300

    final commit

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -10,6 +10,7 @@ const baseUrl = 'http://localhost:8080';
 @Injectable({
   providedIn: 'root'
 })
+
 export class MovimentacaoService {
   constructor(private http: HttpClient) { }
 
@@ -17,4 +18,12 @@ export class MovimentacaoService {
     return this.http.get(`${baseUrl}/movimentacoes`);
   }
 
+  findByIdConta(idConta: any): Observable<any> {
+    return this.http.get(`${baseUrl}/movimentacoes/${idConta}`);
+  }
+
+  create(movimentacao: any): Observable<any> {
+    return this.http.post(`${baseUrl}/movimentacoes`, movimentacao);
+  }
 }
+
